e8447956e98896cc5c7b7f3747a2af9e4b0690ba
phamminhtan2002 <tanphamminh2002@gmail.com>
2023-08-31 06:30:42 -0700

    readme

--- src/lib/getBasePath.ts
+++ src/lib/getBasePath.ts
@@ -2,7 +2,7 @@ const getBasePath = () => {
   let base_url =
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000'
-      : process.env.VERCEL_URL
+      : `https://${process.env.VERCEL_URL}`
   return base_url
 }
 
