17c891ab16f4e910bbcb21e7dc009feb9b118320
phamminhtan2002 <tanphamminh2002@gmail.com>
2023-09-02 21:00:10 -0700

    enable chatgpt

--- src/app/location/[city]/[lat]/[long]/page.tsx
+++ src/app/location/[city]/[lat]/[long]/page.tsx
@@ -10,7 +10,7 @@ import cleanData from '@/lib/cleanData'
 import getBasePath from '@/lib/getBasePath'
 import React from 'react'
 
-export const revalidate = 1440
+export const revalidate = 0
 
 type Props = {
   params: {
@@ -36,20 +36,27 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
 
   const dataToSend = cleanData(results, city)
 
-  const res = await fetch(`${getBasePath()}/api/getWeatherSummary`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      weatherData: dataToSend,
-    }),
-  })
+  let content: string
 
-  const GPTdata = await res.json()
-  const { content } = GPTdata
+  try {
+    const res = await fetch(`${getBasePath()}/api/getWeatherSummary`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        weatherData: dataToSend,
+      }),
+    })
 
-  // const content="Good morning, everyone! This is Garen, coming to you LIVE from the AI Weather Forecast. Today, we have an exciting summary of the weather in Burnaby! Currently, we have a temperature of 13.1°C with a gentle breeze blowing at 4.4 km/h from the southwest. You might want to grab a light jacket before heading out. Now, let's dive into the hourly weather for today. The temperatures will range from a pleasant 20.3°C during the afternoon to 13.1°C in the evening. So, it's going to be a day of mild temperatures, perfect for outdoor activities! Here's a little joke to brighten up your day: Why did the weather go to therapy? Because it had too many highs and lows! But wait, there's more! We have a low chance of rain throughout the day, with just a spritz of 0.6 mm expected in the afternoon. So, no need to worry about carrying an umbrella. However, the relative humidity will be on the higher side, peaking at 100% in the morning. So, you might want to keep an eye on your hair if you're stepping out early. Now, let's talk about the UV index. It's important to protect your skin from harmful rays! Today, the UV index will start off at a moderate level of 3.1 in the morning. As the day progresses, it will reach its peak at 5.35 in the afternoon. Make sure to apply sunscreen, wear sunglasses, and seek shade when necessary to keep your skin safe and glowing! That's it for your weather summary, Burnaby! Remember to dress accordingly, enjoy your day, and stay weather-wise. Back to you in the studio!"
+    const GPTdata = await res.json()
+    // const { content } = GPTdata
+    content = GPTdata.content
+  } catch (error) {
+    console.log(error)
+    content =
+      "Good morning, everyone! This is Garen, coming to you LIVE from the AI Weather Forecast. Today, we have an exciting summary of the weather in Burnaby! Currently, we have a temperature of 13.1°C with a gentle breeze blowing at 4.4 km/h from the southwest. You might want to grab a light jacket before heading out. Now, let's dive into the hourly weather for today. The temperatures will range from a pleasant 20.3°C during the afternoon to 13.1°C in the evening. So, it's going to be a day of mild temperatures, perfect for outdoor activities! Here's a little joke to brighten up your day: Why did the weather go to therapy? Because it had too many highs and lows! But wait, there's more! We have a low chance of rain throughout the day, with just a spritz of 0.6 mm expected in the afternoon. So, no need to worry about carrying an umbrella. However, the relative humidity will be on the higher side, peaking at 100% in the morning. So, you might want to keep an eye on your hair if you're stepping out early. Now, let's talk about the UV index. It's important to protect your skin from harmful rays! Today, the UV index will start off at a moderate level of 3.1 in the morning. As the day progresses, it will reach its peak at 5.35 in the afternoon. Make sure to apply sunscreen, wear sunglasses, and seek shade when necessary to keep your skin safe and glowing! That's it for your weather summary, Burnaby! Remember to dress accordingly, enjoy your day, and stay weather-wise. Back to you in the studio!"
+  }
 
   return (
     <div className='flex flex-col min-h-screen md:flex-row'>
