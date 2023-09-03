71cb933a30b7acae4c6c106eb79b354e8f20d8b6
phamminhtan2002 <tanphamminh2002@gmail.com>
2023-09-02 22:10:46 -0700

    enable chatgpt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { Suspense } from 'react'
 import './globals.css'
-import Loading from './loading'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
